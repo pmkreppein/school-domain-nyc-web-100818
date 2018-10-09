534c49fcf5b7830ce969b575a986e59b4af74c9b
Peter Kreppein <pmkreppein@gmail.com>
2018-10-09 21:30:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+end
+  def add_student(student, grade)
+    @roster[student] = []
+    @roster[student] << grade
+end
+end
